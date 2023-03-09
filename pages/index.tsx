6a75223535b2b2a76a67b89311f55f6492b43364
PDF-Origami <pdforigami@gmail.com>
2023-03-09 14:49:50 +0200

    Add links to licence and source

--- pages/index.tsx
+++ pages/index.tsx
@@ -145,12 +145,30 @@ export default function Home() {
             />
           </div>
         </div>
-        <p className="text-zinc-500 text-xs">
-          Made by{" "}
-          <a href="https://twitter.com/PDFOrigami" className="text-blue-600">
-            @PDFOrigami
-          </a>
-        </p>
+        <div className="flex">
+          <p className="text-zinc-500 text-xs">
+            Made by{" "}
+            <a href="https://twitter.com/PDFOrigami" className="text-blue-600">
+              @PDFOrigami
+            </a>
+            {" • "}
+            Source:{" "}
+            <a
+              href={`https://en.wikipedia.org/wiki/AD_${data?.year}`}
+              className="text-blue-600"
+            >
+              Wikipedia
+            </a>
+            {" • "}
+            <a
+              href="https://creativecommons.org/licenses/by-sa/3.0/"
+              className="text-blue-600"
+            >
+              Licence
+            </a>
+          </p>
+          <p className="text-zinc-500 text-xs"></p>
+        </div>
       </main>
     </>
   );
